50e9275b5c5bd5ed85eb6bcb7e5fa0bc8bc0fc46
Rishal-KV <rishkv923@gmail.com>
2024-06-28 00:16:16 +0530

    ui changes

--- src/Components/StudentComponent/MainChat.tsx
+++ src/Components/StudentComponent/MainChat.tsx
@@ -96,7 +96,7 @@ function MainChat({
         className="w-8 h-8 rounded-full"
       /> */}
               </div>
-              {chats.text.includes("http://localhost:5173/video") ? (
+              {chats.text.includes("https://paper-pencil.vercel.app/video") ? (
                 <div className="flex flex-col items-center">
                   {/* <button
                     className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
